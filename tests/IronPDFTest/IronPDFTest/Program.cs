f306daef0cbcccb6cad51408f6a8874c88972552
Michel Kansou <michel.kansou@outlook.fr>
2017-06-24 18:06:16 +0200

    :white_check_mark: IronPDF

---
+++ tests/IronPDFTest/IronPDFTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IronPdf;
+
+
+namespace IronPDFTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            HtmlToPdf HtmlToPdf = new IronPdf.HtmlToPdf();
+            HtmlToPdf.RenderHtmlAsPdf("<p>hello world</p>").SaveAs(@"" + path + "/File.Pdf");
+        }
+    }
+}
