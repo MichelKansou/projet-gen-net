e4321c279d64a325f032e08829bf13a2ec2d3720
Michel Kansou <michel.kansou@outlook.fr>
2017-06-24 18:19:26 +0200

    :white_check_mark: create text file

---
+++ tests/TextFileTest/TextFileTest/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextFileTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            path += @"/test.txt";
+            if (!File.Exists(path))
+            {
+                // Create a file to write to.
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.Write("Hello kf,aezopfkaofkazopfkazpodkaz opdaz,d opaz, dopaz, dopaz,d opze f,zeop f,zeopf, z,ap");
+                }
+            }
+
+            // Open the file to read from.
+            using (StreamReader sr = File.OpenText(path))
+            {
+                string s = "";
+                while ((s = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+        }
+    }
+}
